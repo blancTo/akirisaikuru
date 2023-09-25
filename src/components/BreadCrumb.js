a9ed9039a2e478c9fab936433510250eed87e71d
blancTo <git@blanc.to>
2023-09-26 06:40:37 +0900

    ブログ記事等に対応したパンくずの作成、修正

--- src/components/BreadCrumb.js
+++ src/components/BreadCrumb.js
@@ -1,32 +1,46 @@
-import React from 'react'
-import { Link } from "gatsby"
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { faHouse,faAngleRight } from '@fortawesome/free-solid-svg-icons'
+import React from "react";
+import { Link } from "gatsby";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHouse, faAngleRight } from "@fortawesome/free-solid-svg-icons";
 
-const BreadCrumb = (props) => {    
-    const siteurl = "https://aki-risaikuru.com/";
+const BreadCrumb = (props) => {
+  const siteurl = "https://aki-risaikuru.com/";
   return (
     <>
-        <div id="breadcrumb">
-            <ul className="breadcrumb__list" itemscope itemtype="https://schema.org/BreadcrumbList">
-                <li className="breadcrumb__item" itemprop="itemListElement" itemscope itemtype="https://schema.org/ListItem">
-                    <FontAwesomeIcon icon={faHouse} />
-                    <Link to={siteurl} itemprop="item">
-                        <span itemprop="name">ホーム</span>
-                    </Link>
-                    <meta itemprop="position" content="1" />
-                </li>                    
-                <li className="breadcrumb__item" itemprop="itemListElement" itemscope itemtype="https://schema.org/ListItem">
-                    <FontAwesomeIcon icon={faAngleRight} />
-                    <Link to={siteurl + props.slug + '/'} itemprop="item">
-                        <span>{props.title}</span>
-                    </Link>
-                    <meta itemprop="position" content="2" />
-                </li>
-            </ul>
-        </div>
-    </>
-  )
-}
+      <div id="breadcrumb">
+        <ul
+          className="breadcrumb__list"
+          itemscope
+          itemtype="https://schema.org/BreadcrumbList"
+        >
+          <li
+            className="breadcrumb__item"
+            itemprop="itemListElement"
+            itemscope
+            itemtype="https://schema.org/ListItem"
+          >
+            <FontAwesomeIcon icon={faHouse} />
+            <Link to={siteurl} itemprop="item">
+              <span itemprop="name">ホーム</span>
+            </Link>
+            <meta itemprop="position" content="1" />
+          </li>
+          <li
+            className="breadcrumb__item"
+            itemprop="itemListElement"
+            itemscope
+            itemtype="https://schema.org/ListItem"
+          >
+            <FontAwesomeIcon icon={faAngleRight} />
 
-export default BreadCrumb
+            <span>{props.title}</span>
+
+            <meta itemprop="position" content="2" />
+          </li>
+        </ul>
+      </div>
+    </>
+  );
+};
+
+export default BreadCrumb;
---
+++ src/components/BreadCrumbBlog.js
@@ -0,0 +1,61 @@
+import React from "react";
+import { Link } from "gatsby";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHouse, faAngleRight } from "@fortawesome/free-solid-svg-icons";
+
+const BreadCrumbBlog = (props) => {
+  const siteurl = "https://aki-risaikuru.com/";
+
+  return (
+    <div id="breadcrumb">
+      <ul
+        className="breadcrumb__list"
+        itemscope
+        itemtype="https://schema.org/BreadcrumbList"
+      >
+        <li
+          className="breadcrumb__item"
+          itemprop="itemListElement"
+          itemscope
+          itemtype="https://schema.org/ListItem"
+        >
+          <FontAwesomeIcon icon={faHouse} />
+          <Link to={siteurl} itemprop="item">
+            <span itemprop="name">ホーム</span>
+          </Link>
+          <meta itemprop="position" content="1" />
+        </li>
+        {props.category && (
+          <li
+            className="breadcrumb__item"
+            itemprop="itemListElement"
+            itemscope
+            itemtype="https://schema.org/ListItem"
+          >
+            <FontAwesomeIcon icon={faAngleRight} />
+            <Link to={`/blog/${props.category}/`} itemprop="item">
+              <span>{props.catName}</span>
+            </Link>
+            <meta itemprop="position" content="2" />
+          </li>
+        )}
+        {props.post && ( // props.postが存在する場合のみ以下の<li>要素を表示
+          <li
+            className="breadcrumb__item"
+            itemprop="itemListElement"
+            itemscope
+            itemtype="https://schema.org/ListItem"
+          >
+            <FontAwesomeIcon icon={faAngleRight} />
+            <Link to={`/posts/${props.post}/`} itemprop="item">
+              <span>{props.title}</span>
+            </Link>
+            <meta itemprop="position" content="3" />
+          </li>
+        )}
+      </ul>
+    </div>
+  );
+};
+
+export default BreadCrumbBlog;
