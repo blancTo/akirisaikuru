560c4b83e9ef3050587a15cf574f6863c4f69796
blancTo <git@blanc.to>
2023-09-26 06:37:13 +0900

    SEOコンポーネントをキャメルケースへ変更
    SEO→Seo

--- src/components/Seo.js
+++ src/components/Seo.js
@@ -1,27 +1,31 @@
-import React from "react"
+import React from "react";
 
-import { useStaticQuery, graphql } from "gatsby"
-import { useLocation } from "@reach/router"
-import PropTypes from "prop-types"
+import { useStaticQuery, graphql } from "gatsby";
+import { useLocation } from "@reach/router";
+import PropTypes from "prop-types";
 
-const SEO = ({ title, title2, description,keyword, image, article }) => {
-  const { site } = useStaticQuery(query)
+const Seo = ({ title, title2, description, keyword, image, article }) => {
+  const { site } = useStaticQuery(query);
   const {
     defaultTitle,
     defaultDescription,
     defaultKeyword,
     siteUrl,
     defaultImage,
-  } = site.siteMetadata
-  const { pathname } = useLocation()
+  } = site.siteMetadata;
+  const { pathname } = useLocation();
 
   const seo = {
-    title: title ? `${title} | ${defaultTitle}` : title2 ? `${title2}` : defaultTitle,
+    title: title
+      ? `${title} | ${defaultTitle}`
+      : title2
+      ? `${title2}`
+      : defaultTitle,
     keyword: keyword || defaultKeyword,
     description: description || defaultDescription,
     image: `${siteUrl}${image || defaultImage}`,
     siteUrl: `${siteUrl}${pathname}`,
-  }
+  };
 
   return (
     <>
@@ -36,17 +40,17 @@ const SEO = ({ title, title2, description,keyword, image, article }) => {
         <meta property="og:description" content={seo.description} />
       )}
       {seo.image && <meta property="og:image" content={seo.image} />}
-      <meta name="twitter:card" content="summary_large_image" />      
+      <meta name="twitter:card" content="summary_large_image" />
       {seo.title && <meta name="twitter:title" content={seo.title} />}
       {seo.description && (
         <meta name="twitter:description" content={seo.description} />
       )}
       {seo.image && <meta name="twitter:image" content={seo.image} />}
     </>
-  )
-}
+  );
+};
 
-export default SEO
+export default Seo;
 
 const query = graphql`
   query {
@@ -60,12 +64,12 @@ const query = graphql`
       }
     }
   }
-`
+`;
 
-SEO.propTypes = {
+Seo.propTypes = {
   title: PropTypes.string,
   description: PropTypes.string,
   keyword: PropTypes.string,
   image: PropTypes.string,
   article: PropTypes.bool,
-}
+};
